307687051e96b5234fd7083d7bbd692b15ec6c9c
Schimpf-Christian <christian.schimpf@jku.at>
2019-03-12 18:05:32 +0100

    Included basic projects and NuGet Packages

---
+++ PCFS/Model/RelayCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PCFS.Model
+{
+
+    public class RelayCommand<T> : ICommand
+    {
+        private Action<T> _execute;
+        private Predicate<T> _canExcecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<T> excec, Predicate<T> canExec)
+        {
+            _execute = excec;
+            _canExcecute = canExec;
+        }
+
+        public RelayCommand(Action<T> act) : this(act, null)
+        {
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExcecute == null ? true : _canExcecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke((T)parameter);
+        }
+    }
+    
+}
