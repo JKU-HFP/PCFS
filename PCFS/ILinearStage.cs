c8367f14e54d0b59cb79a822d562105ae2117e80
Christian Schimpf <christian.schimpf@jku.at>
2019-03-20 19:23:51 +0100

    Addes Interface

---
+++ PCFS/ILinearStage.cs
@@ -0,0 +1,13 @@
+﻿namespace PIStage_Library
+{
+    public interface ILinearStage
+    {
+        bool ControllerReady { get; }
+
+        void Connect(string filter);
+        bool Move_Absolute(double position);
+        bool Move_Relative(double position);
+        bool SetVelocity(double velocity);
+        bool WaitForPos();
+    }
+}
